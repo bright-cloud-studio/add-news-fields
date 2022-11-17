60caaea4b8e72acf0ea3012cb1986599e736728e
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-17 14:44:22 -0500

    Create autoload.php

---
+++ system/modules/add_news_fields/config/autoload.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Add News Fields
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-news-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+
+/* Register Classes */
+ClassLoader::addClasses(array
+(
+	'CustomTags\AddNewsFieldsTags' 		=> 'system/modules/add_news_fields/library/CustomTags/AddNewsFieldsTags.php'
+));
