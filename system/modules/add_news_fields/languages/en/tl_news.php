6eb8935aa13480c3c9f2a79b3a390daf92bdb74a
Bright Cloud Studio <84467517+bright-cloud-studio@users.noreply.github.com>
2022-11-17 14:49:10 -0500

    Create tl_news.php

---
+++ system/modules/add_news_fields/languages/en/tl_news.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Bright Cloud Studio's Add News Fields
+ *
+ * Copyright (C) 2022-2023 Bright Cloud Studio
+ *
+ * @package    bright-cloud-studio/add-news-fields
+ * @link       https://www.brightcloudstudio.com/
+ * @license    http://opensource.org/licenses/lgpl-3.0.html
+**/
+
+/* Legends */
+$GLOBALS['TL_LANG']['tl_news']['add_news_fields_legend'] = 'Custom Field\'s Settings';
+
+/* Fields */
+$GLOBALS['TL_LANG']['tl_news']['news_issue']          = array('Issue Number', 'Enter this news article\'s issue number');
